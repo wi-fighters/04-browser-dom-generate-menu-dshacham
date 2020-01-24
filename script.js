3d6c58e8e0665ec8e64bb529de35f13853630da9
Dana Shacham <dshacham22@gmail.com>
2020-01-24 16:17:39 +0100

    add finished task

--- script.js
+++ script.js
@@ -3,52 +3,46 @@
     // create an element
     // target its parent
     // prepend (or append) it as a child
-    
+
     // DO THE SETUP
-    
+
     // grab the closest EXISTING parent already in the DOM (on the page)
     const body = document.body;
-    
-    // we're creating a list based on the number of sections, so grab those...
-    
+
     // grab all the sections and destructure it into an array
     const sections = [...document.querySelectorAll('section')];
-    
+
     // map over them and store their id in a new array
-    const ids = sections.map(section => section.id); // home
-    
+    const ids = sections.map(section => section.id);
+
     // DO THE HARD WORK
-    
-    // create a new thing
+
+    // create the new header
     const header = document.createElement('header');
-    
+
     const ul = document.createElement('ul');
-    
+
     // Which tool do we use to iterate over these steps?
     // We don't need a new array, so forEach works for us
-    
-    ids.forEach(id => {
+
+    ids.reverse().forEach(id => {
         const li = document.createElement('li');
-    
+
         const link = document.createElement('a');
-        link.setAttribute('href', '#home'); // TODO: get this programmatically
-        // Maybe use something like...
-        // const hrefs = ids.map(id => '#' + id);
-    
-        const linkText = document.createTextNode('Home'); // TODO: get this programmatically
-    
+
+        link.setAttribute('href', '#' + id); // TODO: get this programmatically
+
+        const linkText = document.createTextNode(id); // TODO: get this programmatically
+
+        // Prepend everything
         link.prepend(linkText);
-    
+
         li.prepend(link);
-    
+
         ul.prepend(li);
     });
-        
+    // Prepend header and body
     header.prepend(ul);
-    
-    // SEE THE RESULT
-    
-    // Once you're built the whole subtree in memory,
-    // Add it to the DOM (define the relationship between the two things)
-    body.prepend(header);    
+
+    body.prepend(header);
 })();
